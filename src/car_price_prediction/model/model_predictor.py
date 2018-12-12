f0894f8177012806debcf8e3c2d944960a2cc336
Danchesko <danberdov@gmail.com>
2018-12-12 20:53:16 +0600

    update model predictor

--- src/car_price_prediction/model/model_predictor.py
+++ src/car_price_prediction/model/model_predictor.py
@@ -1,10 +1,16 @@
 from src.car_price_prediction.utils import dataset_manager, model_manager, df_utils, json_manager
 
 
-def predict(test, estimator=model_manager.get_trained_forest_estimator()):
-    X, y = df_utils.get_data_and_target(
-        dataset_manager.get_processed_dataset())
-    test = json_manager.get_test_data(test, X.columns)
-    X, test = df_utils.make_dummies(X, test)
-    prediction = estimator.predict(test)
-    return prediction.tolist()
+class Predictor:
+    
+    def __init__(self, estimator=None):
+        if not estimator:
+            self.estimator = model_manager.get_trained_forest_estimator()
+        else:
+            self.estimator = estimator
+        self.X, self.y = df_utils.get_data_and_target(dataset_manager.get_processed_dataset())
+
+    def predict(self, test):
+        test_data = json_manager.get_test_data(test, self.X.columns)
+        _, test_data = df_utils.make_dummies(self.X, test_data)
+        return self.estimator.predict(test_data).tolist()
