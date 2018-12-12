585f62835d133011f7d23794767a1b8f21917bc4
Danchesko <danberdov@gmail.com>
2018-12-12 17:08:25 +0600

    create api endpoints

--- src/server.py
+++ src/server.py
@@ -1,25 +1,32 @@
 import json
-from flask import Flask, request, render_template, send_from_directory
-from src.index import get_prediction
+from flask import Flask, request, render_template, send_from_directory, make_response
+from src.app import api_service
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/js/<path:path>')
 def serve_js(path):
     return send_from_directory('static', path)
 
 
-@app.route('/api/prediction', methods=['POST'])
+@app.route('/api/v1/prediction', methods=['POST'])
 def prediction():
     if request.method == 'POST':
         data_for_prediction = json.loads(request.data)
-        response = str(get_prediction(data_for_prediction))
+        response = str(api_service.get_prediction(data_for_prediction))
+        return response
+
+
+@app.route('/api/v1/cars')
+def get_cars():
+        data = api_service.get_cars()
+        response = make_response(data.getvalue())
+        response.headers['Content-Disposition'] = "attachment; filename=cars.csv"
+        response.headers['Content-type'] = 'text/csv'
         return response
 
 
