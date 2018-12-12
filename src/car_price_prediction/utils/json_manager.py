fb3065f9958a1b7f02db344d3e7c0c2b8846cba9
Danchesko <danberdov@gmail.com>
2018-12-13 02:01:57 +0600

    update json_manager

--- src/car_price_prediction/utils/json_manager.py
+++ src/car_price_prediction/utils/json_manager.py
@@ -1,12 +1,15 @@
 import pandas as pd
 from argparse import Namespace
+from dateutil.parser import parse
 
 
 def get_test_data(arguments, columns):
     car_params = Namespace(**arguments)
     test_data = pd.DataFrame(data=[[int(car_params.year),
+                                    parse(car_params.publication).toordinal(),
                                     car_params.transmission,
                                     car_params.brand,
+                                    car_params.model,
                                     float(car_params.capacity),
                                     car_params.drive,
                                     float(car_params.mileage),
