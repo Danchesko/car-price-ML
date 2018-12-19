f15a615da030df53697f2d9c0b378dc787aea497
Danchesko <danberdov@gmail.com>
2018-12-19 18:48:07 +0600

    add validators to form

--- src/app/prediction_form.py
+++ src/app/prediction_form.py
@@ -1,15 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, SubmitField, IntegerField, FloatField, validators
+from wtforms.widgets import Input
 
 
 
 class PredictionForm(FlaskForm):
     from src.server import data_service
-    brand = SelectField('Марка', [validators.Required()], choices=data_service.get_brands(), default = data_service.DEFAULT_VALUE)
-    model = SelectField('Модель',choices=[])
-    year = IntegerField('Год выпуска',  [validators.DataRequired()])
-    mileage = IntegerField('Пробег')
-    capacity = FloatField('Объем')
+    brand = SelectField('Марка', [validators.InputRequired()], choices=data_service.get_brands(), default = data_service.DEFAULT_VALUE)
+    model = SelectField('Модель',[validators.InputRequired()], choices=[])
+    year = IntegerField('Год выпуска', [validators.InputRequired()], widget=Input('number'))
+    mileage = IntegerField('Пробег', [validators.InputRequired()], widget=Input('number'))
+    capacity = FloatField('Объем', [validators.InputRequired()], widget=Input('number'))
     wheel = SelectField('Руль', choices=data_service.get_wheel_types(), default = 'левый')
     fuel = SelectField('Тип топлива', choices=data_service.get_fuel_types(), default = 'бензин')
     color = SelectField('Цвет', choices=data_service.get_colors(), default = 'белый')
