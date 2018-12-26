fb4b51289b4cf113d36ee222671e3968fd96a36a
Danchesko <danberdov@gmail.com>
2018-12-26 17:26:41 +0600

    bug fix

--- project-report/models/random_forest_model.py
+++ project-report/models/random_forest_model.py
@@ -6,4 +6,4 @@ class RandomForestModel(Model):
 
     def __init__(self, name, n_estimators=30):
         self.name = name
-        self.regressor = RandomForestRegressor(n_estimators=n_estimators=)
+        self.regressor = RandomForestRegressor(n_estimators=n_estimators)
